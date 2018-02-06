54ebecea9a45b6e10219bd5b1538b5036204f614
Melita Dsouza <dmelita1994@gmail.com>
2018-02-05 20:21:35 -0500

    Update checkPermutation.py

--- checkPermutation.py
+++ checkPermutation.py
@@ -1,26 +1,34 @@
-def checkPermutation(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        if len(s) != len(p):
+def checkPermuation(s1,s2):
+    
+    # Remove spaces and lowercase letters
+    s1 = s1.replace(' ','').lower()
+    s2 = s2.replace(' ','').lower()
+    
+    #check if same number of letters
+    if len(s1) != len(s2):
+        return False
+    
+    # Create counting dictionary
+    count = {}
+   
+    # Fill dictionary for first string (add counts)
+    for letter in s1:
+        if letter in count:
+            count[letter] += 1
+        else:
+            count[letter] = 1
+            
+    # Fill dictionary for second string (subtract counts)
+    for letter in s2:
+        if letter in count:
+            count[letter] -= 1
+        else:
+            count[letter] = 1
+    
+    # Check that all counts are 0
+    for k in count:
+        if count[k] != 0:
             return False
-        
-        count = {}
-        for char in s:
-            if char in count:
-                count[char] += 1
-            else:
-                count = 1
-                
-        for char in p:
-            if char in count:
-                count[char] -= 1
-            else:
-                count = 1
-        
-        for k in count:
-            if count[k] != 0:
-                return False
-        return True
+
+    # Otherwise they're anagrams/permutations 
+    return True

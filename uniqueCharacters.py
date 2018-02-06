0dfb1c5787ab1e0d5d2a9bf00c25ec9b876e0a14
Melita Dsouza <dmelita1994@gmail.com>
2018-02-05 22:08:13 -0500

    Update uniqueCharacters.py

--- uniqueCharacters.py
+++ uniqueCharacters.py
@@ -10,3 +10,28 @@ def uniqueCharacters(s):
             chars.add(letter)
             
     return True
+
+#No data structures used. Complexity - O(nlogn) because the string is sorted first.
+
+def uniqueCharacters2(s):
+    s = sorted(s)
+    if len(s) == 0:
+        return
+    strlength = len(s)
+    for i in range(strlength-1):
+        if s[i]==s[i+1]:
+            return False
+    return True
+
+#No data structres used. Complexity - O(n^2)
+
+def uniqueCharacters3(s):
+   
+    if len(s) == 0:
+        return
+    strlength = len(s)
+    for i in range(strlength-1):
+        for j in range(i+1,strlength-1):
+            if s[i] == s[j]:
+                return False
+    return True
